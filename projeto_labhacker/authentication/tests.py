2b29d411b6b1686c4ccb3a22e71d140479c993c9
Thiago Nogueira <thiagonf10@gmail.com>
2019-08-26 08:53:25 -0300

    Adding test urls from apps

--- projeto_labhacker/authentication/tests.py
+++ projeto_labhacker/authentication/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import Client
 
-# Create your tests here.
+class AuthTest(TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_login_url(self):
+        # Issue a GET request.
+        response = self.client.get('/')
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
+    
+    def test_logout_url(self):
+        # Issue a GET request.
+        response = self.client.get('/auth/logout', follow=True)
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 200)
